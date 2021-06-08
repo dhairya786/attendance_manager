d4d9c309828f71b15498af5370482124f5b83f44
dhairya786 <65353870+dhairya786@users.noreply.github.com>
2021-06-09 00:40:45 +0530

    test

--- role/views.py
+++ role/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .models import Student, Attendance, Course
+from .models import Student, Attendance, Course, Teachers
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from .utils import facestore
