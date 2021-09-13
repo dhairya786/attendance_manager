8889cde23a0b7684addf14f6d9638cb0c7cd391d
dhairya786 <65353870+dhairya786@users.noreply.github.com>
2021-09-13 13:07:49 +0530

    chutya kaata machine ka

--- role/models.py
+++ role/models.py
@@ -29,6 +29,12 @@ FIELD_CHOICES = [
 
 
 
+class Batch(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} and {self.id}"
+
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
